adf3b7e7844a6799e23fdab8f03a43cd98696f56
Igor Yasinsky <iyasinskyy@gmail.com>
2023-11-12 19:05:07 +0200

    Added basename

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="/react-08-phonebook">
           <App />
         </BrowserRouter>
       </PersistGate>
